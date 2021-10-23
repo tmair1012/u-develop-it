fcb94725688311008b5039ea88de8f6b1f9f3823
TylerMair <tyleramair@gmail.com>
2021-10-23 14:52:53 -0700

    query for returning candidates

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,20 @@ const app = express();
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//Connect to DATABASE
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Moxie2020!',
+    database: 'election'
+},
+console.log('Connected to the election database')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 
 
 app.get('/', (req, res) => {
