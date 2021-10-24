540b5e6be442d68ab80d980a6d4c2506c61cff51
TylerMair <tyleramair@gmail.com>
2021-10-24 12:39:52 -0700

    added join methods to API

--- server.js
+++ server.js
@@ -22,7 +22,11 @@ console.log('Connected to the election database')
 
 //Get all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name 
+    AS party_name 
+    FROM candidates 
+    LEFT JOIN parties 
+    ON candidates.party_id = parties.id`;
 
     db.query(sql, (err, rows) => {
         if (err){
@@ -38,7 +42,12 @@ app.get('/api/candidates', (req, res) => {
 
 // GET a single candidates
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id 
+             WHERE candidates.id = ?`;
     const params = [req.params.id];
   
     db.query(sql, params, (err, row) => {
